2f49fd0d2a73e138cb95fb61854718a45896ec79
OparaAnuliAmara <amaraopara33@gmail.com>
2022-06-30 19:09:59 -0700

    sizess

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	char d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", sizeof(a));
 	printf("Size of long int: %lu byte(s)\n", sizeof(b));
 	printf("Size of long long int: %lu byte(s)\n", sizeof(c));
-	printf("Size of float: %lu byte(s)\n", sizeof(d));
+	printf("Size of float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
