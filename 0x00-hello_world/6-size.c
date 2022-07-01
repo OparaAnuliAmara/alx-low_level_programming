c3db98c86a643567c299b061168891ae2092956a
OparaAnuliAmara <amaraopara33@gmail.com>
2022-06-30 19:06:32 -0700

    sizeee

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,9 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(i));
-	printf("Size of long int: %lu byte(s)\n", sizeof(l));
-	printf("Size of long long int: %lu byte(s)\n", sizeof(ll));
+	printf("Size of an int: %lu byte(s)\n", sizeof(a));
+	printf("Size of long int: %lu byte(s)\n", sizeof(b));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of float: %lu byte(s)\n", sizeof(d));
 	return (0);
 }
