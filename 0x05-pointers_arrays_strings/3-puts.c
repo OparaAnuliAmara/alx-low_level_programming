f5e9aa766ef37b211ba52887977322548cd7eed4
OparaAnuliAmara <amaraopara33@gmail.com>
2022-07-12 12:12:46 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
-
 /*
- * _puts - entry point
+ * _puts - prints a string to stdout
+ *
  * @str: string input
- * Return: void
  */
 
 void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != 0; i++)
 	{
 		_putchar(str[i]);
 	}
