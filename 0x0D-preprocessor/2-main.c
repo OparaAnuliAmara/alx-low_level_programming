0022ac4b08e334f2640df5b42e2cabe617955f08
OparaAnuliAmara <amaraopara33@gmail.com>
2022-08-01 05:48:06 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - this function prints the name of the current file
+ *
+ * Return: 0 after succesful run
+ */
+ int main()
 {
 	printf("\nFILE :%s", __FILE__);
+
 	return (0);
 }
